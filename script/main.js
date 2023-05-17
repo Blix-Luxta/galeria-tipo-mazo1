716047bcd5b78455804446ef78d79d4395320c49
Blix-Luxta <blix.aux@gmail.com>
2023-05-16 23:25:34 -0300

    terminando los botones. Deberia de agregar un selectos de fotos y agrandar la foto al seleccionarla como si fuera un mazo

--- script/main.js
+++ script/main.js
@@ -6,26 +6,62 @@ const loveButton = document.getElementById("love-button");
 
 const groups = document.getElementsByClassName("card-group");
 
+//love button
+
 const handleLoveClick = () =>{
     // Bump active index
 
     const nextIndex = activeIndex + 1 <= groups.length - 1 ? activeIndex + 1 : 0;
 
-    const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
-    nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
+    const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`);
+    const nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
 
     //Active group becomes after
 
     currentGroup.dataset.status = "after";
 
     // Next group becomes active
+    nextGroup.dataset.status = "becoming-active-from-before"
 
-    nextGroup.dataset.status = "active";
+    setTimeout(() => {
+        nextGroup.dataset.status = "active";
 
-    activeIndex = nextIndex;
+        activeIndex = nextIndex;
+    });
+
 }
 
 loveButton.addEventListener("click", () => {
     handleLoveClick();
+    console.log("funca");
+})
+
+// Hate Button
+
+const handleHateClick = () =>{
+    // Bump active index
+
+    const previousIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : groups.length - 1;
+
+    const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`);
+    const previousGroup = document.querySelector(`[data-index="${previousIndex}"]`);
+
+    //Active group becomes after
+
+    currentGroup.dataset.status = "before";
+
+    // Next group becomes active
+    previousGroup.dataset.status = "becoming-active-from-after"
+
+    setTimeout(() => {
+        previousGroup.dataset.status = "active";
+
+        activeIndex = previousIndex;
+    });
+
+}
+
+hateButton.addEventListener("click", () => {
+    handleHateClick();
     console.log("funca");
 })
